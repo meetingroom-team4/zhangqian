521325fa4da03f9f4dc058178d99a1522a38cfcd
zhangqians <2545026281@qq.com>
2016-08-06 16:59:28 +0800

    show hello world

---
+++ README.md
@@ -0,0 +1,7 @@
+------------------------
+input:
+'npm install --save lodash'
+'jasmine'
+------------------------
+output:
+6 hello world!
---
+++ src/hw.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhagnian on 16-8-6.
+ */
+let _=require('lodash');
+let a=_.sum([1,2,3]);
+console.log(a);
+console.log("hello world!");
